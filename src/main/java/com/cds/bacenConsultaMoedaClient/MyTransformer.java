83c66fce7856ae2149f47ca4fed98f1354541f2d
conradperes <conrad.marques.peres@gmail.com>
2017-05-11 16:35:48 -0300

    Add files via upload
    Update in My transformer

--- src/main/java/com/cds/bacenConsultaMoedaClient/MyTransformer.java
+++ src/main/java/com/cds/bacenConsultaMoedaClient/MyTransformer.java
@@ -14,25 +14,72 @@
  * permissions and limitations under the License.
  *
  */
-package com.cds.bacenConsultaMoedaClient;
+package com.cds.pcrj.bacenConsultaMoedaSemAxisIntegration;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.StringReader;
+import java.io.StringWriter;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+
+import org.eclipse.persistence.jaxb.MarshallerProperties;
 import org.springframework.stereotype.Component;
+
+import com.thoughtworks.xstream.io.HierarchicalStreamReader;
+import com.thoughtworks.xstream.io.HierarchicalStreamWriter;
+import com.thoughtworks.xstream.io.copy.HierarchicalStreamCopier;
+import com.thoughtworks.xstream.io.json.JettisonMappedXmlDriver;
+import com.thoughtworks.xstream.io.xml.XppReader;
+import com.utilities.jaxb.DatabaseInventory;
 
 /**
  * A sample transform
  */
 @Component(value = "myTransformer")
 public class MyTransformer {
+	static String convertedJsonFile = "/convertedFile.json";
 
-    public String transform() {
-        // let's return a random string
-        StringBuffer buffer = new StringBuffer();
-        for (int i = 0; i < 3; i++) {
-            int number = (int) (Math.round(Math.random() * 1000) % 10);
-            char letter = (char) ('0' + number);
-            buffer.append(letter);
-        }
-        return buffer.toString();
-    }
+	public String transform() {
+		// lets return a random string
+		StringBuffer buffer = new StringBuffer();
+		for (int i = 0; i < 3; i++) {
+			int number = (int) (Math.round(Math.random() * 1000) % 10);
+			char letter = (char) ('0' + number);
+			buffer.append(letter);
+		}
+		return buffer.toString();
+	}
+
+	public String parseXmlToJson(String xml) {
+		HierarchicalStreamReader sourceReader = new XppReader(new StringReader(xml));
+		StringWriter buffer = new StringWriter();
+		JettisonMappedXmlDriver jettisonDriver = new JettisonMappedXmlDriver();
+		jettisonDriver.createWriter(buffer);
+		HierarchicalStreamWriter destinationWriter = jettisonDriver.createWriter(buffer);
+		destinationWriter.startNode("soapenv:Envelope");
+		HierarchicalStreamCopier copier = new HierarchicalStreamCopier();
+		copier.copy(sourceReader, destinationWriter);
+		return buffer.toString();
+	}
+
+
+//	public void xmlToJson(String xmlFile) throws Exception {
+//		File xml = new File(xmlFile);
+//		JAXBContext jc = JAXBContext.newInstance(DatabaseInventory.class);
+//		Unmarshaller unmarshaller = jc.createUnmarshaller();
+//		DatabaseInventory activity = (DatabaseInventory) unmarshaller.unmarshal(xml);
+//
+//		Marshaller marshaller = jc.createMarshaller();
+//		marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+//		marshaller.setProperty(MarshallerProperties.MEDIA_TYPE, "application/json");
+//		marshaller.setProperty(MarshallerProperties.JSON_INCLUDE_ROOT, true);
+//		FileOutputStream fos = new FileOutputStream(new File(convertedJsonFile));
+//		marshaller.marshal(activity, fos);
+//		fos.close();
+//
+//	}
 
 }
